d5f3788b4223e8b069cac1ef93a526230565cf52
GT_MAC_2 <fengli@fenglideMacBook-Pro.local>
2015-07-21 11:42:45 +0800

    Initial Commit

---
+++ qqqqqqqqq/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  qqqqqqqqq
+//
+//  Created by GT_MAC_2 on 15/7/21.
+//  Copyright (c) 2015年 hyc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
